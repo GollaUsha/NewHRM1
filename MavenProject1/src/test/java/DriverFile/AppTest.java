88d27b3790941f2bf2911b029458765fa4a2f37c
Gollausha <anushasri68154@gmail.com>
2018-12-28 00:45:01 -0800

    HRM!

---
+++ MavenProject1/src/test/java/DriverFile/AppTest.java
@@ -0,0 +1,23 @@
+package DriverFile;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void StartApp() throws Throwable {
+		//System.setProperty("webdriver.chrome.driver", "CommonJarFiles/chromedriver.exe");
+	WebDriver	driver= new ChromeDriver();
+	driver.manage().deleteAllCookies();
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.get("http://orangehrm.qedgetech.com/symfony/web/index.php/auth/login");
+	System.out.println("added");
+		
+	}
+
+}
